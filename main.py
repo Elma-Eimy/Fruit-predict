6506dea348afee0df57930813ec46e2bf92210bb
Elma-Eimy <2075274894@qq.com>
2025-05-18 20:27:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,6 +42,36 @@ def predict_image(file_name, model_path='train_models/best_model.pth'):
     print(f'预测类别为：{fruit_dict[predicted_class]},可信度为：{poss:.4f}')
 
 
+# 用于处理同一个文件夹下的所有图片
+def predict_image_butch(folder_name, model_path='train_models/best_model.pth'):
+    Device = torch.cuda.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    predict_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    image_paths = glob.glob(os.path.join(folder_name, "*[jp][pn]g"))
+    batch_image = []
+    for image_path in image_paths:
+        image = Image.open(image_path).convert("RGB")
+        batch_image.append(predict_transform(image))
+    batch_tensor = torch.stack(batch_image)
+
+    model = new_train.create_model(fine_tune=True, num_classes=len(class_names))
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    with torch.no_grad():
+        outputs = model(batch_tensor)
+        probs, predicted_classes = torch.max(torch.softmax(outputs, dim=1), dim=1)
+
+    for i in range(len(predicted_classes)):
+        print(f"预测水果种类为：{fruit_dict[class_names[predicted_classes[i]]]}，具体为：{class_names[predicted_classes[i]]}，"
+              f"可信度为：{probs[i]:.4f}；源图片：{os.path.basename(image_paths[i])}")
+
+
 if __name__ == '__main__':
     predict_image(file_name='fruit_image/application_image/aaa1.jpg')
 
